9e24af02b4397efb059417159dbb20243ae0f9c5
Eric Li <ekli@wpi.edu>
2026-02-02 19:47:10 -0500

    Fixed writing screen ios nav for gesture

--- composeApp/src/commonMain/kotlin/com/lemurs/lemurs_app/ui/screens/WritingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/lemurs/lemurs_app/ui/screens/WritingScreen.kt
@@ -1,6 +1,7 @@
 package com.lemurs.lemurs_app.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +24,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -114,9 +117,16 @@ fun WritingScreen(onNavigateTo: (String) -> Unit) {
     }
     // --- End of Weekly Question Logic ---
 
+    val focusManager = LocalFocusManager.current
+
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            }
     ) {
         Column(
             modifier = Modifier
